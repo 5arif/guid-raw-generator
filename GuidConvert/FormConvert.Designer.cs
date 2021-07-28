a38ce093a36e28124f7309c1f0b792aec4ae00ee
Sarif Hidayat <sarif.hidayat@indocyber.co.id>
2021-07-28 15:56:41 +0700

    'init'

---
+++ GuidConvert/FormConvert.Designer.cs
@@ -0,0 +1,131 @@
+﻿
+namespace GuidConvert
+{
+    partial class FormConvert
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.btnConvert = new System.Windows.Forms.Button();
+            this.btnNew = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Input";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 42);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Result";
+            // 
+            // txtInput
+            // 
+            this.txtInput.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtInput.Location = new System.Drawing.Point(79, 10);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(336, 23);
+            this.txtInput.TabIndex = 2;
+            // 
+            // txtResult
+            // 
+            this.txtResult.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtResult.Location = new System.Drawing.Point(79, 38);
+            this.txtResult.Name = "txtResult";
+            this.txtResult.ReadOnly = true;
+            this.txtResult.Size = new System.Drawing.Size(336, 23);
+            this.txtResult.TabIndex = 3;
+            // 
+            // btnConvert
+            // 
+            this.btnConvert.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnConvert.Location = new System.Drawing.Point(340, 67);
+            this.btnConvert.Name = "btnConvert";
+            this.btnConvert.Size = new System.Drawing.Size(75, 23);
+            this.btnConvert.TabIndex = 4;
+            this.btnConvert.Text = "Convert";
+            this.btnConvert.UseVisualStyleBackColor = true;
+            this.btnConvert.Click += new System.EventHandler(this.btnConvert_Click);
+            // 
+            // btnNew
+            // 
+            this.btnNew.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnNew.Location = new System.Drawing.Point(259, 67);
+            this.btnNew.Name = "btnNew";
+            this.btnNew.Size = new System.Drawing.Size(75, 23);
+            this.btnNew.TabIndex = 5;
+            this.btnNew.Text = "New Guid";
+            this.btnNew.UseVisualStyleBackColor = true;
+            this.btnNew.Click += new System.EventHandler(this.btnNew_Click);
+            // 
+            // FormConvert
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(427, 104);
+            this.Controls.Add(this.btnNew);
+            this.Controls.Add(this.btnConvert);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FormConvert";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Tibero Convert Guid";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.TextBox txtResult;
+        private System.Windows.Forms.Button btnConvert;
+        private System.Windows.Forms.Button btnNew;
+    }
+}
+
---
+++ GuidConvert/FormConvert.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace GuidConvert
+{
+    public partial class FormConvert : Form
+    {
+        public FormConvert()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConvert_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                txtInput.Text = txtInput.Text?.Trim().ToUpper();
+                if (txtInput.Text.Contains("-"))
+                {
+                    var isValid = Guid.TryParse(txtInput.Text, out Guid guidOutput);
+                    if (isValid)
+                    {
+                        txtResult.Text = BitConverter.ToString(guidOutput.ToByteArray()).Replace("-", string.Empty);
+                    }
+                }
+                else
+                {
+                    var guid = new Guid(txtInput.Text);
+                    var newBytes = new byte[16];
+                    var oldBytes = guid.ToByteArray();
+
+                    for (var i = 8; i < 16; i++)
+                        newBytes[i] = oldBytes[i];
+
+                    newBytes[3] = oldBytes[0];
+                    newBytes[2] = oldBytes[1];
+                    newBytes[1] = oldBytes[2];
+                    newBytes[0] = oldBytes[3];
+                    newBytes[5] = oldBytes[4];
+                    newBytes[4] = oldBytes[5];
+                    newBytes[6] = oldBytes[7];
+                    newBytes[7] = oldBytes[6];
+
+                    txtResult.Text = new Guid(newBytes).ToString().ToUpper();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }   
+        }
+
+        private void btnNew_Click(object sender, EventArgs e)
+        {
+            var guid = Guid.NewGuid();
+            txtInput.Text = guid.ToString();
+
+            btnConvert_Click(sender, e);
+        }
+    }
+}
